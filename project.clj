eadaea33bd27bc21d9211a65ff1afe3fdecddbb0
Peter Fraenkel <pnf@podsnap.com>
2014-07-16 10:25:30 -0400

    Reorganize and add datomic properties.

--- project.clj
+++ project.clj
@@ -1,6 +1,17 @@
-(defproject temporal "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject acyclic.temporal "0.1.0-SNAPSHOT"
+  :author "Peter Fraenkel <http://podsnap.com>"
+  :description "Management of inhomogenous temporal data"
+  :url "http://github.com/pnf/temporal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [digest "1.4.4"]
+                 [clj-time "0.7.0"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [com.datomic/datomic-pro "0.9.4766.11" :exclusions [[org.slf4j/log4j-over-slf4j]]]
+                 [com.taoensso/timbre "3.2.0"] 
+                 [org.clojure/tools.reader "0.8.5"]
+                 [org.clojure/tools.cli "0.3.1"]]
+  :aot [acyclic.temporal.testutils.bitemp]
+  :uberjar-name "temporal.jar"
+)
